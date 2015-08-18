2555b062192a38f0e3f5981f58a3e96bdf154a8e
Danielle Cushing <daniellecushing@Danielles-MacBook-Pro.local>
2015-08-18 16:58:00 -0400

    more mailer changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
    config.action_mailer.raise_delivery_errors = false
-   config.action_mailer.default_url_options = { host: 'cryptic-plateau-4200.heroku.com' }
-   config.action_mailer.delivery_method = :smtp
+   #config.action_mailer.default_url_options = { host: 'cryptic-plateau-4200.heroku.com' }
+   #config.action_mailer.delivery_method = :smtp
    config.action_mailer.smtp_settings = {
      address: ENV['MAILER_ADDRESS'],
      port: '587',
